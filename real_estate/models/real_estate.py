45a0a17b65948c9ce57723ee7a9b5a368838eafd
cvh-odoo <cvh@odoo.com>
2023-01-30 12:00:19 +0100

    CVH test 5
    - added data
    - added description & first field

--- real_estate/models/real_estate.py
+++ real_estate/models/real_estate.py
@@ -1,4 +1,7 @@
-from odoo import models
+from odoo import fields, models
 
 class RealEstate(models.Model):
     _name = "real.estate"
+    _date = "It's the model that defines what a house is"
+
+    name = fields.Char()
