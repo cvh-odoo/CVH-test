0560fe4d49b6ff2862f6d3f0c8160e71f86b1a7e
cvh-odoo <cvh@odoo.com>
2023-01-30 11:08:56 +0100

    Add real estate test 1
    check if module in Apps

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Real Estate Custom',
+    'version': '1',
+    'category': 'Custom',
+    'sequence': 15,
+    'summary': 'Custom Real estate',
+    'depends': [
+        'base_setup',
+    ],
+    'data': [
+
+        'views/real_estate.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
