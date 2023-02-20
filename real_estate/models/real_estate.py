0cfe20085ab5b60875f675bef73bd644ff017260
cvh-odoo <cvh@odoo.com>
2023-02-20 10:24:05 +0100

    Add fields on model
    Add fields on model

--- real_estate/models/real_estate.py
+++ real_estate/models/real_estate.py
@@ -5,3 +5,15 @@ class RealEstate(models.Model):
     _date = "It's the model that defines what a house is"
 
     name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availibility = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(string='Type', selection=[('north','North'), ('south','South'), ('east','East'), ('west','West')], help="Choose your garden orientation")
